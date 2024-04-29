ce1b9edf039ef7bd26939ac2b1040f05c355dc02
raincheny <saier_cy@163.com>
2024-04-29 16:32:44 +0800

    modify 2

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Pycharm 常用快捷键
 Ctrl + Alt + L  # 选中一段代码自动调整缩进格式
 Ctrl + Shift + N # 通过文件名快速查找工程内的文件
+Alt + Shift + Enter # 导入引用
 
 PyCharm的调试模式
 F9：进入调试模式选择框，可以选择进行调试的脚本（）
--- 数据类型.py
+++ 数据类型.py
@@ -1,17 +1,25 @@
+import random
+
 # 科学计数表示法表示浮点数
 print(1.23e9)
 print(1.2e-3)
 
 # 十六进制数
 print(0xff00)
-
-# 六进制
 print(0x770)
 
-# 转义符
-print('I\'m \"OK\"!')
-print(r'我\n是\t陈\\云')
+# 八进制
+print(0o12)  # 零加上小写欧再加数字
 
+# 二进制
+print(0b1010)
 
-
-
+# 随机函数
+print(random.choice([2, 3, 4, 33, 22, 66, 5, 7, 4]))  # 从列表（也可以是元组、字符串）中获取一个随机内容
+print(random.randrange(1, 100, 2))  # 从 1-100 中选取一个奇数，2表示递增基数
+print(random.randrange(100))  # 从 0-99 选取一个随机数
+print(random.random())  # 返回随机生成的一个实数，它在[0,1)范围内
+lista = [20, 16, 10, 5]
+random.shuffle(lista)  # 将序列的所有元素随机排序
+print("随机排序列表 : ", lista)
+print(random.uniform(5, 10))  # 返回一个浮点数 N，取值范围为如果 x<y 则 x <= N <= y，如果 y<x 则y <= N <= x
